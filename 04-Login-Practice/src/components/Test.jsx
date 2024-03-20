7f6f5e5acb3f59b6abb0dafbaf6d39d8cfad6f99
mohamed reda <moreda2722gmail.com>
2024-03-20 16:15:19 +0200

    starting new setion in react course

--- 04-Login-Practice/src/components/Test.jsx
+++ 04-Login-Practice/src/components/Test.jsx
@@ -9,6 +9,7 @@ margin: 21px auto;
 cursor: pointer;
 user-select:none`
 export default function Test() {
+    const [toggler, setToggler] = useState(false);
     const [isValidValue, setIsValidValue] = useState(true)
     return (
         <>
@@ -17,6 +18,9 @@ export default function Test() {
             }}>
                 hello this's just testing text "Click On Me"
             </TestingElement>
+            <p style={{color: toggler?'red':'white', textAlign:'center', fontWeight: 'bold', fontSize: '27px', textTransform: 'capitalize', userSelect: 'none', cursor: 'pointer'}} onClick={()=>{
+                setToggler(!toggler)
+            }}>hello this's inline style</p>
         </>
     )
 }
